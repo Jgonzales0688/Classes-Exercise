2b33e077a333bce1d56280d3257eb5d9a28ea42b
Jessica Gonzales <jgonzales0688@gmail.com>
2022-12-08 18:48:37 -0600

    Exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -3,8 +3,16 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {   //Exercise 1
+            Car car1 = new Car();
+                
+            car1.Make = "Chevy";
+            car1.Model = "Z06 Corvette";
+            car1.Year = 2008;
+
+            Console.WriteLine($"At the car show we saw a {car1.Year}, {car1.Make} {car1.Model}.");
+            
+            
         }
     }
 }
